3e641784a5f1c1e161b0ab65dd95d1d3a6c559b2
vermadev54 <vermadev54@gmail.com>
2022-08-30 10:04:22 +0000

    integration test

--- allconcept/integraton-test/test_docker.py
+++ allconcept/integraton-test/test_docker.py
@@ -11,73 +11,34 @@ with open('event.json', 'rt', encoding='utf-8') as f_in:
 
 url = 'http://localhost:8080/2015-03-31/functions/function/invocations'
 actual_response = requests.post(url, json=event).json()
-print('actual response:')
 
+print('actual response:')
 print(json.dumps(actual_response, indent=2))
 
 expected_response = {
-    'predictions': [
-        {
-            'model': 'ride_duration_prediction_model',
-            'version': 'Test123',
-            'prediction': {
-                'ride_duration': 21.3,
-                'ride_id': 256,
-            },
-        }
-    ]
+  "predictions": [
+    {
+      "model": "Credit_Card_Churn_Prediction",
+      "version": "f1ba7bfd01624384bb4f2ac6f2741d98",
+      "prediction": {
+        "Churn_Prediction": "Existing Customer",
+        "profile_id": 10
+      }
+    }
+  ]
 }
+
+print('expected_response:')
+print(json.dumps(expected_response, indent=2))
 
 
 diff = DeepDiff(actual_response, expected_response, significant_digits=1)
 print(f'diff={diff}')
 
-#assert 'type_changes' not in diff
-#assert 'values_changed' not in diff
+assert 'type_changes' not in diff
+assert 'values_changed' not in diff
+
+print("test_docker_all_done")
 
 
-# import json
 
-# import requests
-# from deepdiff import DeepDiff
-
-
-# data={
-#  'Customer_Age': 55,
-#  'Gender': 'F',
-#  'Dependent_count': 3,
-#  'Education_Level': 'Uneducated',
-#  'Marital_Status': 'Single',
-#  'Income_Category': 'Unknown',
-#  'Card_Category': 'Blue',
-#  'Months_on_book': 47,
-#  'Total_Relationship_Count': 4,
-#  'Months_Inactive_12_mon': 3,
-#  'Contacts_Count_12_mon': 3,
-#  'Credit_Limit': 14657.0,
-#  'Total_Revolving_Bal': 2517,
-#  'Avg_Open_To_Buy': 12140.0,
-#  'Total_Amt_Chng_Q4_Q1': 0.166,
-#  'Total_Trans_Amt': 6009,
-#  'Total_Trans_Ct': 53,
-#  'Total_Ct_Chng_Q4_Q1': 0.514,
-#  'Avg_Utilization_Ratio': 0.172}
-
-# print("input data",json.dumps(data, indent=2))
-# url = 'http://localhost:8001/predict'
-# actual_response = requests.post(url, json=data)
-# #print('actual response:',actual_response.json())
-# actual_response=actual_response.json()
-
-# print("actual_response",json.dumps(actual_response, indent=2))
-
-# expected_response= {'Attrition_Flag_predicted': 'Attrited Customer', 'statusCode': 200}
-
-# print("expected_response",json.dumps(expected_response, indent=2) )
-
-
-# diff = DeepDiff(actual_response, expected_response, significant_digits=1)
-# print(f'diff={diff}')
-
-# assert 'type_changes' not in diff
-# assert 'values_changed' not in diff
